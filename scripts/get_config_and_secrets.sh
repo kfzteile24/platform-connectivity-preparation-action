ff0ed8967d1cda382db16652d657fe30a2ef9c30
platform-code <ibrahim.alkurdi@kfzteile24.de>
2021-04-27 17:02:34 +0200

    [DEV-26292] add action for platform preparation

---
+++ scripts/get_config_and_secrets.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euox pipefail
+
+# Config and secrets preparation:
+mkdir -p kube-config k8s/${PLATFORM_ID}
+export VAULT_ADDR="http://secrets.kfz42.de"
+
+# Loing to Vault and pick the platform secrets file and K8s config file:
+vault login -method=token token=$VAULT_GIT_TOKEN > /dev/null 2>&1
+vault kv get -field=value secret/${PLATFORM_ENV}/${KUBECONFIG} > ${KUBECONFIG}
+vault kv get -field=value secret/${PLATFORM_ENV}/k8s/${PLATFORM_ID}/platform-secrets.yaml >  k8s/${PLATFORM_ID}/platform-secrets.yaml
+
+# Pick Config from Platform-code and global environment secrets from Vault:
+if [[ $PLATFORM_ENV == "aws.live" ]];then
+  ## get vault stage secrets
+  vault kv get -field=value secret/${PLATFORM_ENV}/k8s/${PLATFORM_ID}/stage-secrets.yaml > k8s/${PLATFORM_ID}/stage-secrets.yaml
+  ## get stage and platform config
+  curl -Ls -o k8s/${PLATFORM_ID}/stage-config.yaml https://${GIT_TOKEN}@raw.githubusercontent.com/${PLATFORM_CODE_VALUES_DIR}/${PLATFORM_ENV}/${PLATFORM_ID}/stage-config.yaml
+  curl -Ls -o k8s/${PLATFORM_ID}/platform-config.yaml https://${GIT_TOKEN}@raw.githubusercontent.com/${PLATFORM_CODE_VALUES_DIR}/${PLATFORM_ENV}/${PLATFORM_ID}/platform-config.yaml
+
+elif [[ $PLATFORM_ENV == "aws.qa" ]];then
+  ## get vault stage secrets
+  vault kv get -field=value secret/${PLATFORM_ENV}/k8s/stage-secrets.yaml > k8s/${PLATFORM_ID}/stage-secrets.yaml
+  ## get stage and platform config
+  curl -Ls -o k8s/${PLATFORM_ID}/stage-config.yaml https://${GIT_TOKEN}@raw.githubusercontent.com/${PLATFORM_CODE_VALUES_DIR}/${PLATFORM_ENV}/stage-config.yaml
+  curl -Ls -o k8s/${PLATFORM_ID}/platform-config.yaml https://${GIT_TOKEN}@raw.githubusercontent.com/${PLATFORM_CODE_VALUES_DIR}/${PLATFORM_ENV}/${PLATFORM_ID}/platform-config.yaml
+fi
+
+# List all the config and secrets files:
+ls -l k8s/${PLATFORM_ID}
+ls -l kube-config
---
+++ scripts/setup_env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update -y && sudo apt-get install -y openvpn screen python3.6-venv python3.6-dev\
+&& sudo pip install boto wheel
+
+# Install helm
+curl -Ls -o helm.tar.gz "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz" \
+  && tar -xvf helm.tar.gz \
+  && chmod +x linux-amd64/helm \
+  && sudo mv linux-amd64/helm /usr/local/bin/helm \
+  && rm helm.tar.gz
+
+# Install kubectl
+curl -Ls -o kubectl "https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl" \
+  && chmod +x kubectl \
+  && sudo mv kubectl /usr/local/bin/kubectl
+
+# Install Vault client
+curl -Ls -O "https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip" \
+&& unzip vault_${VAULT_VERSION}_linux_amd64.zip \
+&& chmod +x vault \
+&& sudo mv vault /usr/local/bin \
+&& rm vault_${VAULT_VERSION}_linux_amd64.zip
+
+# Install aws-iam-authenticator
+curl -Ls -o aws-iam-authenticator "https://amazon-eks.s3-us-west-2.amazonaws.com/${AWS_IAM_AUTH_VERSION}/2021-01-05/bin/linux/amd64/aws-iam-authenticator"
+chmod +x aws-iam-authenticator
+sudo mv aws-iam-authenticator /usr/local/bin
+
+# Install Hostess
+curl -Ls -o  hostess "https://github.com/cbednarski/hostess/releases/download/v${HOSTESS_VERSION}/hostess_linux_amd64"
+chmod +x hostess
+sudo mv hostess /usr/local/bin/
