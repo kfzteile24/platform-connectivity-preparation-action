a80fb6055ead54e1cfbfa3aea2ddef497950ef6a
Dennis Hoppe <dennis.hoppe@dhoppe.it>
2021-10-14 13:37:35 +0200

    Remove output via ls -la

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -16,10 +16,8 @@ curl -Ls -o kubectl "https://storage.googleapis.com/kubernetes-release/release/v
   && sudo mv kubectl /usr/local/bin/kubectl
 
 # Install Vault client
-ls -la
 curl -Ls -O "https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip" \
 && unzip -o vault_${VAULT_VERSION}_linux_amd64.zip \
-&& ls -la \
 && chmod +x vault \
 && sudo mv vault /usr/local/bin \
 && rm vault_${VAULT_VERSION}_linux_amd64.zip
