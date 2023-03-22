1bdc4cfce704e27ea194e19222895a2ffec86cc5
Aleksejs Mihailovs <aleksejs.mihailovs@kfzteile24.de>
2023-03-22 15:20:07 +0100

    Removing access keys. We will use github oidc

--- scripts/connect_to_k8s.sh
+++ scripts/connect_to_k8s.sh
@@ -4,9 +4,6 @@ set -euox pipefail
 export KUBECTL="kubectl --kubeconfig=${KUBECONFIG}"
 alias kubectl="$KUBECTL"
 
-export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
-export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
-
 # Check k8s connectivity:
 if [[ $(kubectl cluster-info |echo $?) -ne 0 ]]; then
   echo "Unable to connect to k8s ..."
