79eafc58bf653d83052ac7b78f46858750ff245a
Aleksejs Mihailovs <115465561+aleksk24@users.noreply.github.com>
2024-03-05 12:38:31 +0100

    Fixing version 4.2 (#15)
    * Fixing version 4.2
    
    * Using the beta platform-connectivity-preparation action

--- scripts/connect_to_k8s.sh
+++ scripts/connect_to_k8s.sh
@@ -9,7 +9,7 @@ sudo netstat -nr
 
 mkdir -p kube-config k8s/${PLATFORM_ID}
 
-if [[ -z ${KUBECONFIG_RAW+x} ]]; then
+if [[ -z ${KUBECONFIG_RAW} ]]; then
   echo "${KUBECONFIG_BASE64}" | base64 --decode > ${KUBECONFIG}
 else
   echo "${KUBECONFIG_RAW}" > ${KUBECONFIG}
